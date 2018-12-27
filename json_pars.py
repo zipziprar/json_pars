32e911ac992a59648eb10e27dbb1cb94dc052421
timtimbot <timtimbot@gmail.com>
2018-12-27 17:36:59 +0300

    Initial commit

---
+++ json_pars.py
@@ -0,0 +1,9 @@
+import json
+from urllib.request import urlopen
+
+
+url = urlopen('https://raw.githubusercontent.com/zemirco/sf-city-lots-json/master/citylots.json')
+obj = json.load(url)
+data = [street for street in obj['features'] if street['properties']['STREET'] == 'JEFFERSON']
+data = sorted(data, key=lambda k: k['properties']['LOT_NUM'], reverse=True)
+print(data)
